32f0b990b92b81001062c832143d5d698cdb5af1
dungdinhhaha <dungdinhhaha@users.noreply.github.com>
2026-09-22 01:54:08 +0700

    Use browser navigation for protected downloads

--- client/src/utils/download.utils.js
+++ client/src/utils/download.utils.js
@@ -1,25 +1,20 @@
 import request from "./request.utils";
 
-const getFileNameFromDisposition = (disposition = "", fallback = "skillhub-file") => {
-  const utf8Match = disposition.match(/filename\*=UTF-8''([^;]+)/i);
-  if (utf8Match?.[1]) return decodeURIComponent(utf8Match[1]);
-  const asciiMatch = disposition.match(/filename="?([^";]+)"?/i);
-  return asciiMatch?.[1] || fallback;
+const getApiFileUrl = (path) => {
+  if (/^https?:\/\//i.test(path)) return path;
+  const baseUrl = request.defaults.baseURL || "";
+  return `${baseUrl.replace(/\/$/, "")}/${String(path).replace(/^\//, "")}`;
 };
 
-const downloadBlob = (blob, fileName) => {
-  const url = URL.createObjectURL(blob);
+const downloadByBrowser = (url) => {
   const link = document.createElement("a");
   link.href = url;
-  link.download = fileName || "skillhub-file";
+  link.rel = "noopener";
   document.body.appendChild(link);
   link.click();
   link.remove();
-  URL.revokeObjectURL(url);
 };
 
 export const downloadProtectedFile = async (url, fallbackName = "skillhub-file") => {
-  const res = await request.get(url, { responseType: "blob" });
-  const fileName = getFileNameFromDisposition(res.headers["content-disposition"], fallbackName);
-  downloadBlob(res.data, fileName);
+  downloadByBrowser(getApiFileUrl(url));
 };
